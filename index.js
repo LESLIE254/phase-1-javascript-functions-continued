3d9a43ef19ebc68b195574530e58ed50a4b1281f
Leslie Ngwono Nyatigi <lesngwono84@gmail.com>
2022-08-17 09:58:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+
+function saturdayFun (activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+const mondayWork = function (activity2 = "go to the office") {
+    return `This Monday, I will ${activity2}.`
+}
+mondayWork();
+mondayWork("go swimming");
+
+function wrapAdjective (flair){
+    const innerFunction = function (msg = "special"){
+        return `You are ${flair}${msg}${flair}!`;
+    }
+    return innerFunction;
+
+}
+wrapAdjective("*");
+wrapAdjective("%")("dedicated programmer");
